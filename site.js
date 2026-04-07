e4346bb04740f034a58fa62b0462a21f4be39229
Valerie Gaigg <300102994@fvtc.edu>
2026-04-06 22:22:36 -0500

    updated nav

--- site.js
+++ site.js
@@ -1,38 +1,15 @@
-const elements = document.querySelectorAll('.scroll-animation-element');
-const elements2 = document.querySelectorAll('.scroll-mywork');
-const project = document.querySelector('#project');
-const block = document.querySelectorAll('.orange');
-const h1 = document.querySelector('h1');
+const prj1 = document.getElementById('prj1');
+const prj2 = document.getElementById('prj2');
+const prj3 = document.getElementById('prj3');
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('is-visible');
-            // Optional: Unobserve the element after the animation plays once
-            observer.unobserve(entry.target);
-        }
-    });
-}, {
-    root: null, // observe against the viewport
-    threshold: 0.2 // trigger when 10% of the element is visible
+
+prj1.addEventListener('click', () => {
+    window.location.href = "fittrack.html";
 });
-
-elements.forEach(element => {
-    observer.observe(element);
+prj2.addEventListener('click', () => {
+    window.location.href = "fittrack.html";
 });
-
-elements2.forEach(element => {
-    observer.observe(element);
+prj3.addEventListener('click', () => {
+    window.location.href = "fittrack.html";
 });
-
-function dropdown (){
     
-    block.style.display = "true"
-    console.log("dropdown")
-}
-
-function uploadFiles() {
-    const input = document.getElementById('fileInput');
-    const files = input.files
-    
-}
