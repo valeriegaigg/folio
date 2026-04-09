a1634aa5e8281a31e85ca443d46a36826d39ce82
Valerie Gaigg <300102994@fvtc.edu>
2026-04-08 21:00:25 -0500

    big change

--- site.js
+++ site.js
@@ -1,15 +1,28 @@
 const prj1 = document.getElementById('prj1');
 const prj2 = document.getElementById('prj2');
 const prj3 = document.getElementById('prj3');
+const card = document.getElementsByClassName('card');
+const msg = document.getElementsByClassName('msg');
 
 
-prj1.addEventListener('click', () => {
-    window.location.href = "fittrack.html";
-});
-prj2.addEventListener('click', () => {
-    window.location.href = "fittrack.html";
-});
-prj3.addEventListener('click', () => {
-    window.location.href = "fittrack.html";
-});
+function Redirect(url){
+    window.location.href = url;
+}
+
+// prj1.addEventListener('click', () => {
+//     window.location.href = "fittrack.html";
+// });
+// prj2.addEventListener('click', () => {
+//     window.location.href = "fittrack.html";
+// });
+// prj3.addEventListener('click', () => {
+//     window.location.href = "fittrack.html";
+// });
     
+
+// function View(id){
+//         card[id].classList.toggle('view-card');
+//         msg[id].classList.toggle('close');
+// }
+
+
